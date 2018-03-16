fb1a4fdffdfba1fc47271639a1d47269b61f584f
Juan Beccaceci <juanbeccaceci@icloud.com>
2018-03-15 22:41:22 -0300

    Update Platform.h
    Modify Platform.h

--- src/Platform/Platform.h
+++ src/Platform/Platform.h
@@ -5,18 +5,18 @@
 * @version:     1.0.0
 *
 **/
-#ifndef PLATAFORM_H
-#define PLATAFORM_H
+#ifndef PLATFORM_H
+#define PLATFORM_H
 
 ////////////////////////////////////////////////////////////
-/// Plataform types
+/// Platform types
 ////////////////////////////////////////////////////////////
 #define PLATFORM_WINDOWS	0x00
 #define PLATFORM_LINUX		0x01
 #define PLATFORM_MACOS		0x02
 
 ////////////////////////////////////////////////////////////
-/// Check plataform
+/// Check Platform
 ////////////////////////////////////////////////////////////
 #if defined(linux) || defined(__linux)
 #   define PLATFORM_TYPE PLATFORM_LINUX
@@ -25,7 +25,7 @@
 #elif defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh)
 #	define PLATFORM_TYPE PLATFORM_MACOS
 #else
-#	error Plataform error
+#	error Platform error
 #endif
 
-#endif //<-- PLATAFORM_H
+#endif //<-- PLATFORM_H
